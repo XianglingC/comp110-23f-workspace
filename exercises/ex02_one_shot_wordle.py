e90ca27c9eef176d5627c125dc2f0a51d554dd40
XianglingC <cxlmia@ad.unc.edu>
2023-09-19 17:39:18 -0400

    EX02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,28 +3,22 @@
 __author__ = "730658974"
 
 # part 1.
-
-
 secret_word: str = "python"
-
-guess_word: str = input(f"What is your " + str(len(secret_word)) + "-letter guess? ")
-
-
+guess_word: str = input(f"What is your " + {len(secret_word)} + "-letter guess? ")
 
 # part 2. Chencking indices.
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
+# Check the number of characters.
+while len(guess_word) != len(secret_word):
+    guess_word = input(f"That was not " + {len(secret_word)} + " letters! Try again: ")
+
+# Part 2.
 word_idx: int = 0
 emoji: str = ""
 alternate_idx: int = 0
-
-# Check the number of characters.
-while len(guess_word) != len(secret_word):
-    guess_word: str = input(f"That was not " + str(len(secret_word)) + " letters! Try again: ")
-
-# Part 2.
 while word_idx < len(secret_word):
     if guess_word[word_idx] == secret_word[word_idx]:
         emoji += GREEN_BOX
@@ -39,7 +33,7 @@ while word_idx < len(secret_word):
             else:
                 alternate_idx = alternate_idx + 1
         # If will run only when the above while is wrong.
-        if character_existence == True:
+        if character_existence is True:
             emoji += YELLOW_BOX
         else:
             emoji += WHITE_BOX
@@ -48,7 +42,7 @@ while word_idx < len(secret_word):
 print(emoji)
   
 if guess_word != secret_word:
-    print ("Not quite. Play again soon!")
+    print("Not quite. Play again soon!")
     exit()
 
 else:
