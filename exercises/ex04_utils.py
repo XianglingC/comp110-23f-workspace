41f0274f69c7ff989315258d796c6497ea225aea
XianglingC <cxlmia@ad.unc.edu>
2023-10-09 18:46:38 -0400

    EX.04 FINAL

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,9 +1,10 @@
-""" The ex04 practice of list."""
+"""The ex04 practice of list."""
 __author__: str = "730658974"
 
-# 1. all
 
-def all(input_list: list[int], integer: int)-> bool:
+# 1. all
+def all(input_list: list[int], integer: int) -> bool:
+    """To check whether all the int in lists are the same."""
     list_idx: int = 0
     if len(input_list) == 0:
         return False
@@ -14,30 +15,35 @@ def all(input_list: list[int], integer: int)-> bool:
             list_idx += 1
     return True
 
+
 # 2. max
-def max(input: list[int])-> int:
+def max(input: list[int]) -> int:
+    """To find the maximum number in the list."""
     if len(input) == 0: 
         raise ValueError("max() arg is an empty List")
-    list_idx: int = 0
-    while list_idx < len (input):
-        if input[list_idx] > input[list_idx +1]:
-            response: int = input[list_idx]
-        list_idx =+ 1
-    return response
+    list_idx: int = 1
+    max_value = input[0]
+    while list_idx < len(input):
+        if max_value < input[list_idx]:
+            max_value = input[list_idx]
+        list_idx += 1
+    return max_value
+
 
 # 3. is_equal
 # The implementation should not assume the lengths of each list are equal???
 def is_equal(input1: list[int], input2: list[int]) -> bool:
+    """Find whether the number in lists are all the same."""
     idx_1: int = 0
     idx_2: int = 0
     response: bool = True
     if len(input1) != len(input2):
         return False
-    while idx_1 < len(input1) and response :
+    while idx_1 < len(input1) and response:
         if input2[idx_2] == input1[idx_1]:
-            response: bool = True
+            response = True
             idx_1 += 1
             idx_2 += 1
         else:
-            response: bool = False
+            response = False
     return response
