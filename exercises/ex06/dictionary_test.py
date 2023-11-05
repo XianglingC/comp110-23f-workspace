725fdd8d4287335b0a6a2e66e58f37048bc1c67f
XianglingC <cxlmia@ad.unc.edu>
2023-11-04 21:58:24 -0400

    hhfinal again

--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -45,39 +45,39 @@ def test_edge_case() -> None:
 
 #---------------------------------------------
 # The unit test for COUNT FUNCTION
-# TEST1
-def test_use_case1() -> None:
+# COUNT_TEST1
+def test_count_use_case1() -> None:
     """unit test7."""
     list1: list[str] = ["Mom", "Dad", "Mom"]
     assert count(list1) == {"Mom": 2, "Dad": 1}
 
-# TEST2
-def test_use_case2() -> None:
+# COUNT_TEST2
+def test_count_use_case2() -> None:
     """unit test8."""
     list2: list[str] = ["Mom", "Dad", "Mom", "Dad"]
     assert count(list2) == {"Mom": 2, "Dad": 2}
 
-# TEST3
-def test_edge_case() -> None:
+# COUNT_TEST3
+def test_count_edge_case() -> None:
     """unit test9."""
     list3: list[str] = ()
     assert count(list3) == {}
 
 #------------------------------------
 # The unit test for ALPHABETIZER FUNCTION
-# TEST1
+# COUNT_TEST1
 def test_alphabetizer_use_case1() -> None:
     """unit test10."""
     list1: list[str] = ["cat", "apple", "boy", "angry", "bad", "car"]
     assert alphabetizer(list1) == {"c": ["cat", "car"], "a": ["apple", "angry"], "b": ["boy", "bad"]}
 
-# TEST2
+# COUNT_TEST2
 def test_alphabetizer_use_case2() -> None:
     """unit tes11."""
     list2: list[str] = ["hello", "bye"]
     assert alphabetizer(list2) == {"h": ["hello"], "b": ["bye"]}
 
-# TEST3
+# COUNT_TEST3
 def test_alphabetizer_edge_case() -> None:
     """unit test12."""
     list3: list[str] = ["Python", "sugar", "Turtle", "party", "table"]
@@ -85,19 +85,19 @@ def test_alphabetizer_edge_case() -> None:
 
 #-------------------------------------------------------
 # The unit test for UPDATE ATTENDANCE FUNCTION
-# TEST1
+# COUNT_TEST1
 def test_update_attendance_use_case1() -> None:
     """unit test13."""
     attendance_log: dict = {"Monday": ["Vrinda", "Kaleb"], "Tuesday": ["Alyssa"]}
     assert update_attendance(attendance_log, "Tuesday" , "Vrinda") == {"Monday": ["Vrinda", "Kaleb"], "Tuesday": ["Alyssa", "Vrinda"]}
 
-# TEST2
+# COUNT_TEST2
 def test_update_attendance_use_case2() -> None:
     """unit test14."""
     attendance_log: dict = {"Monday": ["Vrinda", "Kaleb"], "Tuesday": ["Alyssa"]}
     assert update_attendance(attendance_log, "Wednesday", "Kaleb") == {"Monday": ["Vrinda", "Kaleb"], "Tuesday": ["Alyssa"], "Wednesday": ["Kaleb"]}
 
-# TEST3
+# COUNT_TEST3
 def test_update_attendance_edge_case() -> None:
     """unit test15."""
     attendance_log: dict = {"Monday": ["Anna"]}
